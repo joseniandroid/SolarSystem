bd402668e74bb5f0f9596c212cb94fb1c0ff92dc
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2016-01-07 13:42:41 +0800

    Initial project structure.

---
+++ app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package sidespell.tech.solarsystem.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import sidespell.tech.solarsystem.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
