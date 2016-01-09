e76d118eee59311989472f8c0b5df0818508eac7
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2016-01-10 00:09:45 +0800

    Created the user interface.

--- app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
+++ app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
@@ -3,16 +3,10 @@ package sidespell.tech.solarsystem.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import sidespell.tech.solarsystem.R;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
